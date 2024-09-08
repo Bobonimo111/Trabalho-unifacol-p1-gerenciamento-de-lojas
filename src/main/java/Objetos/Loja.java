4ff03ccd0eae788bd5eb38c5d5855cdc294c5927
William Rodrigues <carlosmaycon443@gmail.com>
2024-09-08 12:21:08 -0300

    adicionando encapsulamento

--- src/main/java/Objetos/Loja.java
+++ src/main/java/Objetos/Loja.java
@@ -2,13 +2,69 @@ package Objetos;
 
 import Tipos.Tipo;
 
-public class Loja
-{
+public class Loja {
     private String endereco;
     private String codigo;
     private String nome_fantasia;
     private String telefone;
     private Tipo tipo;
     private String cnpj;
+
+    public Loja(String endereco, String codigo, String nome_fantasia, String telefone, Tipo tipo, String cnpj) {
+        this.endereco = endereco;
+        this.codigo = codigo;
+        this.nome_fantasia = nome_fantasia;
+        this.telefone = telefone;
+        this.tipo = tipo;
+        this.cnpj = cnpj;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome_fantasia() {
+        return nome_fantasia;
+    }
+
+    public void setNome_fantasia(String nome_fantasia) {
+        this.nome_fantasia = nome_fantasia;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
 
 }
--- src/main/java/Objetos/Vendedor.java
+++ src/main/java/Objetos/Vendedor.java
@@ -2,15 +2,41 @@ package Objetos;
 
 import Tipos.Turno;
 
-public class Vendedor extends Pessoa{
+public class Vendedor extends Pessoa {
     private int ctps;
     private double salario;
     private Turno turno;
-    Vendedor(String nome, String data, String endereco, String telefone,String cpf,int ctps, double salario,Turno turno){
-        super(nome,data,endereco,telefone,cpf);
+
+    Vendedor(String nome, String data, String endereco, String telefone, String cpf, int ctps, double salario,
+            Turno turno) {
+        super(nome, data, endereco, telefone, cpf);
         this.ctps = ctps;
         this.salario = salario;
         this.turno = turno;
-
     }
+
+    public int getCtps() {
+        return ctps;
+    }
+
+    public void setCtps(int ctps) {
+        this.ctps = ctps;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public Turno getTurno() {
+        return turno;
+    }
+
+    public void setTurno(Turno turno) {
+        this.turno = turno;
+    }
+
 }
